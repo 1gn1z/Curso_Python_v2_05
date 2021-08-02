9cbce50d28a7db1f2d329ebf675ec4612dd3f4b4
1gn1z_el_angel <kebor327@gmail.com>
2021-08-01 19:27:26 -0500

    5.3. 1. clase int

---
+++ 5.3_clase_int.py
@@ -0,0 +1,7 @@
+print('Uso de la clase int() para crear números enteros:')
+
+entrada = int(input('Escriba su edad: '))
+print('Tipo de dato de la variable entrada: %s' %type(entrada))
+
+total_dias = entrada * 365
+print('Total de días vividos hasta el momento: %i' %total_dias)
