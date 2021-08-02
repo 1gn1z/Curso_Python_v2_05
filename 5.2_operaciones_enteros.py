83d34481a8c9bfcfb19008853df5d0ebbbca4279
1gn1z_el_angel <kebor327@gmail.com>
2021-08-01 19:12:05 -0500

    5.2. 1. uso del placeholder %i para enteros

---
+++ 5.2_operaciones_enteros.py
@@ -0,0 +1,20 @@
+# Números enteros:
+
+edad = 28
+x = -5
+puntaje = 300
+
+print(type(edad))
+print(type(x))
+
+print(edad)
+print(x)
+
+# Con estos datos podemos hacer todo tipo de operaciones aritmeticas
+# queremos añadirle 50 a puntaje:
+
+print()
+# %i es un placeholder para ENTEROS.
+print('Puntaje antes de la adición: %i' %puntaje)
+puntaje = puntaje + 50
+
