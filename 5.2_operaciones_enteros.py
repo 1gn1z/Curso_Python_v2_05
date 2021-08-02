0075f11205438b5cfad600740ad0472df0ba8c62
1gn1z_el_angel <kebor327@gmail.com>
2021-08-01 19:15:29 -0500

    5.2. 2. implementacion de += en lugar de x + x =

--- 5.2_operaciones_enteros.py
+++ 5.2_operaciones_enteros.py
@@ -15,6 +15,12 @@ print(x)
 
 print()
 # %i es un placeholder para ENTEROS.
-print('Puntaje antes de la adición: %i' %puntaje)
+print('Puntaje antes de la adición de 50 puntos: %i' %puntaje)
 puntaje = puntaje + 50
+print('Puntaje después de la adición de 50 puntos: %i' %puntaje)
 
+# Se pueden hacer: +,-,*,/,% (resudio) y ** (exponenciación)
+
+x = x + 10 # forma normal
+
+x += 10    # forma mas chida :3
