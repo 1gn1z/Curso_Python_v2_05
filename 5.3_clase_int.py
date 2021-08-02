713f43164c7def9bde93e00d5324e2eaa977b1b6
1gn1z_el_angel <kebor327@gmail.com>
2021-08-01 19:28:17 -0500

    5.3. 2. cambio de nombre de variable entrada por edad para mas claridad

--- 5.3_clase_int.py
+++ 5.3_clase_int.py
@@ -1,7 +1,7 @@
 print('Uso de la clase int() para crear números enteros:')
 
-entrada = int(input('Escriba su edad: '))
-print('Tipo de dato de la variable entrada: %s' %type(entrada))
+edad = int(input('Escriba su edad: '))
+print('Tipo de dato de la variable edad: %s' %type(edad))
 
-total_dias = entrada * 365
+total_dias = edad * 365
 print('Total de días vividos hasta el momento: %i' %total_dias)
