bddc30362078ebc096b7d595c570ce58d47e2b9c
1gn1z_el_angel <kebor327@gmail.com>
2021-08-04 23:06:12 -0500

    5.7. 1. numeros complejos con y sin funcion complex()

---
+++ 5.7_complejos_complex.py
@@ -0,0 +1,5 @@
+# Números complejos:
+
+numero_complejo = 2 - 3j
+print("Contenido variable compleja:", numero_complejo)
+print("El tipo de dato de la variable numero_complejo es ", type(numero_complejo))
