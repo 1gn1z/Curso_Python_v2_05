743b91f468dbb982e4f3a5a6ca8c2073648ac050
1gn1z_el_angel <kebor327@gmail.com>
2021-08-04 23:25:45 -0500

    5.8. 4. multiplicacion logica AND

--- 5.8_booleanos_bool.py
+++ 5.8_booleanos_bool.py
@@ -15,7 +15,23 @@ print(llueve)
 
 # Al principio era Flase (no llueve), pero invertimos el valor lógico
 # asi que la condicional imprime "el día está lluvioso"
+
 if llueve:
     print('El día está lluvioso.')
 else:
     print('El día NO está lluvioso.')
+
+print()
+
+# Operaciones con variables o valores booleanos (lógicos):
+
+a = True
+b = False
+
+# Operadores lógicos: and (conjunción - y), or (disyunción - o)
+print(a and b)
+
+# and es multiplicacion lógica (ambas deben ser true para que arroje true)
+
+print(a and not b)
+# Aqui obtenemos true por que not b = true
