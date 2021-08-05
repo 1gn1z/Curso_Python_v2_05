4519d8528a8bb006c1658131773e4594e2c65655
1gn1z_el_angel <kebor327@gmail.com>
2021-08-04 23:22:02 -0500

    5.8. 3. condicional if else

--- 5.8_booleanos_bool.py
+++ 5.8_booleanos_bool.py
@@ -12,3 +12,10 @@ print()
 llueve = not llueve
 print(type(llueve))
 print(llueve)
+
+# Al principio era Flase (no llueve), pero invertimos el valor lógico
+# asi que la condicional imprime "el día está lluvioso"
+if llueve:
+    print('El día está lluvioso.')
+else:
+    print('El día NO está lluvioso.')
