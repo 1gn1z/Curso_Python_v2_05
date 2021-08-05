bc273dc344fe5047824992052d40bd0545f5a42f
1gn1z_el_angel <kebor327@gmail.com>
2021-08-04 23:08:29 -0500

    5.7. 2. suma de números complejos

--- 5.7_complejos_complex.py
+++ 5.7_complejos_complex.py
@@ -3,3 +3,14 @@
 numero_complejo = 2 - 3j
 print("Contenido variable compleja:", numero_complejo)
 print("El tipo de dato de la variable numero_complejo es ", type(numero_complejo))
+
+print()
+
+numero_complejo = complex(2, -3)
+
+# Operaciones básicas con variables de tipo de dato simple complejo
+
+print('Operaciones aritméticas sobre números complejos:')
+
+suma = 2 - 3j + (1 +5j)
+print("Suma: ",suma)
