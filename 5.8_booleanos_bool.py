6b42767730dc0e90cc843463b9747aba80344e78
1gn1z_el_angel <kebor327@gmail.com>
2021-08-04 23:52:37 -0500

    5.8. 7. refactorizacion de variables con F2 (en vscode)

--- 5.8_booleanos_bool.py
+++ 5.8_booleanos_bool.py
@@ -25,34 +25,34 @@ print()
 
 # Operaciones con variables o valores booleanos (lógicos):
 
-a = True
-b = False
+llave_1 = True
+llave_2 = False
 
 # Operadores lógicos: and (conjunción - y), or (disyunción - o)
-print(a and b)
+print(llave_1 and llave_2)
 
 # and es multiplicacion lógica (ambas deben ser true para que arroje true)
 
-print(a and not b)
+print(llave_1 and not llave_2)
 # Aqui obtenemos true por que not b = true
 
 # OR
 # or es suma lógica (ambos deben ser false para que arroje false)
 print()
-print(b or a)
-print(b or not a)
+print(llave_2 or llave_1)
+print(llave_2 or not llave_1)
 
 print()
 print()
 
-if a and not b:
+if llave_1 and not llave_2:
     print('Sí hay awa')
 else:
     print('No hay awa')
 
 print()
 
-if a or b:
+if llave_1 or llave_2:
     print('Sí hay awa')
 else:
     print('No hay awa')
@@ -60,3 +60,12 @@ else:
 # con AND, AMBAS deben cumplirse para que de TRUE, de lo contrario dará FALSE.
 
 #con OR, SOLO UNA debe cumplirsr para que de TRUE, solo si ambas no se cumplen dará FALSE.
+
+print( )
+
+if not llave_1 or llave_2:
+    print('Sí hay awa')
+else:
+    print('No hay awa')
+
+# Refactorización para renombrar variables    
