e48d67691379bfc97e18eaf69a0f3c8423012442
1gn1z_el_angel <kebor327@gmail.com>
2021-08-05 23:57:27 -0500

    5.9. 1. formas de strings

---
+++ 5.9_cadena_caracteres_string.py
@@ -0,0 +1,13 @@
+# Tipo de dato compuesto string
+
+nombre = "Kebor Mastah"
+email = "kebor327@gmail.com"
+
+varias_lineas = ''' Con triples comillas
+ya sean dobles o sencillas, podemos usar varias
+lineas, como en este ejemplo :3
+'''
+
+print(nombre)
+print(email)
+print(varias_lineas)
