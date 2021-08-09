2d5a1a0bdfaf767d318c5ece95c98957e19d654d
1gn1z_el_angel <kebor327@gmail.com>
2021-08-08 23:01:34 -0500

    6.0. 7. iteración de la tupla con ciclo WHILE

--- 6.0_tuplas_tuples.py
+++ 6.0_tuplas_tuples.py
@@ -39,3 +39,18 @@ x = punto[0]
 y = punto[1]
 print('El valor en x es igual a:', x)
 print('El valor en y es igual a:', y)
+
+print()
+
+# Iteración de un objeto tipo tupla (tuple):
+
+numeros_primos = (2, 3, 5, 7, 11, 13, 17, 19)
+
+print('Cantidad de elementos de la tupla numeros_primos', len(numeros_primos))
+
+# Iteración con un ciclo While:
+i = 0   # esta variable será el índice que recorrerá todos  los elementos de la tupla
+while i < len(numeros_primos):  # menor que porque si ponemos menor o igual, no nos daría el último elemento  
+    print(f'El valor del elemento en el índice {i} es igual a {numeros_primos[i]}.')
+    i += 1  # incrementamos en 1 el valor de i para que avance en los índices, y para NO tener un ciclo infinito.
+
