53da67f42d612d06c63c5a1dc41c2581c65665d9
1gn1z_el_angel <kebor327@gmail.com>
2021-08-08 20:24:25 -0500

    5.9. 13. creacion de funcion propia que hace lo mismo que find()

--- 5.9_cadena_caracteres_string.py
+++ 5.9_cadena_caracteres_string.py
@@ -69,3 +69,16 @@ print('El índice del elemento "1" es igual a', indice)
 
 indice = valores.find('8')
 print('El índice del elemento "8" es igual a', indice)
+
+# Creación de una función (proceso) personalizado para buscar una cadena dentro de otra
+
+def encontrar(cadena, caracter):
+    indice = -1
+    for i in range(0, len(cadena)):
+        if caracter == cadena[i]:
+            indice = i
+            break
+    return indice
+
+indice = encontrar(valores, "2")
+print('El indice del elemento "2" es igual a', indice)
