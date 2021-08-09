eb2ee6982dbc0823251d4d8ca289646791ef678e
1gn1z_el_angel <kebor327@gmail.com>
2021-08-08 21:01:36 -0500

    6.0. 2. acceso a los elementos de la tupla via su indice index

--- 6.0_tuplas_tuples.py
+++ 6.0_tuplas_tuples.py
@@ -4,3 +4,10 @@ punto = (2, 5)
 print('Tipo de dato:', type(punto))
 print('Contenido de la variable punto:', punto)
 print('Cantidad de elementos de la tupla punto:', len(punto))
+
+print()
+# Acceso a los elementos de una tupla:
+x = punto[0]
+y = punto[1]
+print('El valor en x es igual a:', x)
+print('El valor en y es igual a:', y)
