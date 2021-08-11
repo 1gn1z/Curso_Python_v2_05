2c70c26f96d2584615de48aa6151c3f3b667680b
1gn1z_el_angel <kebor327@gmail.com>
2021-08-10 23:18:59 -0500

    6.1. 2. acceso a elementos individuales de la lista

--- 6.1_lista_list.py
+++ 6.1_lista_list.py
@@ -6,3 +6,16 @@ numeros = [2, 4, 6, 8, 10]
 print('Tipo de dato de la variable numeros:', type(numeros))
 print('Cantidad de elementos', len(numeros))
 print('Contenido', numeros) # representación en cadena de caracteres.
+
+print()
+
+#2. Acceso a los elementos de una lista:
+dos = numeros[0]
+diez = numeros[-1]
+print('El primer elemento (índice 0) de la lista es:', dos)
+print('El último elemento (índice -1) de la lista es:', diez)
+
+print()
+
+seccion = numeros[1:4]
+print(seccion)
